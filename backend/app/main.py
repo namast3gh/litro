8486d5a36a1c16bd95c2919bed232cc0e2a4c32a
Кирилл <genger1356@mail.ru>
2025-06-16 22:54:33 +0400

    новые изменения

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:3000",
         "http://87.228.102.111",
+        "http://87.228.102.111:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
